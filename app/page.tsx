76b1f34bc8727bdb8160899eaa91f44094b0d55a
xqy <xqy@Quinns-Mac.local>
2023-10-04 13:11:54 +0700

    add h2

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="h1-bold flex-center">Hello</h1>
+      <h1 className="h2-bold flex-center">World!</h1>
     </div>
   );
 }
