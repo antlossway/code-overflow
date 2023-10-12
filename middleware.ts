cd67589b32c02483166cacffaedbd6d0a79b8104
Quinn Xie <antlossway@gmail.com>
2023-10-12 20:29:24 +0700

    update middleware.ts to allow clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
+import { Webhook } from "svix";
 import { authMiddleware } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
